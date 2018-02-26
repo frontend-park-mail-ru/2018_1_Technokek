829ee07a983a5d3d023a9986c0876fb5f1faa24b
Vitaly Cherkov <vv-ch@bk.ru>
2018-02-26 10:24:34 +0300

    Login Registration forms are swapping

--- public/js/modules/loginRegistrationForms/loginRegistrationForms.js
+++ public/js/modules/loginRegistrationForms/loginRegistrationForms.js
@@ -3,11 +3,13 @@
 const noop = () => null;
 
 class AbstractForm {
-    constructor({selector = 'body', templateFunction = noop, hidden = false} = {}) {
+    constructor({selector = 'body', templateFunction = noop, hidden = false, changeFormSlot = noop} = {}) {
         this._el = document.querySelector(selector);
         this._templateFunction = templateFunction;
         this._hidden = hidden;
         this._updateHidden();
+        this._changeFormSlot = changeFormSlot;
+        this._listenersCreated = false;
     }
 
     get hidden() {
@@ -21,13 +23,21 @@ class AbstractForm {
 
     render() {
         this._el.innerHTML = this._templateFunction();
+        if(!this._listenersCreated)
+            this._createEventListeners();
+    }
+
+    _createEventListeners() {
+        const formChanger = this._el.querySelector('.js-change-form');
+        formChanger.addEventListener('click', this._changeFormSlot);
+        this._listenersCreated = true;
     }
 
     _updateHidden() {
         if(this._hidden)
             this._el.setAttribute('hidden', '');
         else
-            this._el.removeAttribute('hidden', '');
+            this._el.removeAttribute('hidden');
     }
 }
 
@@ -53,17 +63,31 @@ class LoginRegistrationForm {
         }
     }
 
+    changeForms() {
+        if(this._forms) {
+            for(let form of this._forms) {
+                form.hidden = !form.hidden;
+            }
+        }
+    }
+
     _createForms() {
         this._forms = [
             new AbstractForm({
-                selector: '.login-section', 
-                templateFunction: window.loginformTmplTemplate,
-                hidden: false
-            }),
-            new AbstractForm({
                 selector: '.register-section', 
                 templateFunction: window.registrationformTmplTemplate,
-                hidden: true
+                hidden: true,
+                changeFormSlot: () => {
+                    this.changeForms();
+                }
+            }),
+            new AbstractForm({
+                selector: '.login-section', 
+                templateFunction: window.loginformTmplTemplate,
+                hidden: false,
+                changeFormSlot: () => {
+                    this.changeForms();
+                }
             })
         ];
     }
